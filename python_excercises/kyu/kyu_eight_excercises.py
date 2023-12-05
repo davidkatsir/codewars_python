4b9e87b4d62c278af820c7f184ec607d0dc70a46
David Katsir <david.katsir@gmail.com>
2023-12-05 20:52:54 +0200

    Commit number_to_string

--- python_excercises/kyu/kyu_eight_excercises.py
+++ python_excercises/kyu/kyu_eight_excercises.py
@@ -1,5 +1,5 @@
 # ****************************************************************************************************
-print("****************************************************************************************************")
+print("*" * 100)
 
 
 # Even or Odd
@@ -16,7 +16,7 @@ result = even_or_odd(7)
 print(result)  # Output: Odd
 
 # ****************************************************************************************************
-print("****************************************************************************************************")
+print("*" * 100)
 
 
 # Multiply
@@ -29,7 +29,7 @@ multiply_result = multiply(7, 3)
 print(multiply_result)
 
 # ****************************************************************************************************
-print("****************************************************************************************************")
+print("*" * 100)
 
 
 # Sum of positive
@@ -52,7 +52,54 @@ positive_sum_result = positive_sum([-8, 9, 9, 2, -7])
 print(positive_sum_result)  # Output: 20
 
 # ****************************************************************************************************
-print("****************************************************************************************************")
+print("*" * 100)
+
+
+# Reversed Strings
+# Complete the solution so that it reverses the string passed into it.
+# 'world'  =>  'dlrow'
+# 'word'   =>  'drow'
+
+def solution(string):
+    string_to_list = list(string)
+    reversed_string = []
+    for char_in_list in reversed(string_to_list):
+        reversed_string.append(char_in_list)
+    return ''.join(reversed_string)
+
 
 # ****************************************************************************************************
-print("****************************************************************************************************")
+print("*" * 100)
+
+
+# Convert a Number to a String!
+# We need a function that can transform a number (integer) into a string.
+# What ways of achieving this do you know?
+# Examples (input --> output):
+# 123  --> "123"
+# 999  --> "999"
+# -100 --> "-100"
+
+def number_to_string(num):
+    return str(num)
+
+
+# ****************************************************************************************************
+print("*" * 100)
+
+
+# Return Negative
+# In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+# Examples:
+# make_negative(1);  # return -1
+# make_negative(-5); # return -5
+# make_negative(0);  # return 0
+# Notes:
+# The number can be negative already, in which case no change is required.
+# Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+def make_negative(number):
+
+
+# ****************************************************************************************************
+print("*" * 100)
