0670729038aa0f57fe413bfe44866b0f3a0ef935
David Katsir <david.katsir@gmail.com>
2023-12-05 02:43:22 +0200

    Commit positive_sum

--- python_excercises/kyu/kyu_eight_excercises.py
+++ python_excercises/kyu/kyu_eight_excercises.py
@@ -1,3 +1,7 @@
+# ****************************************************************************************************
+print("****************************************************************************************************")
+
+
 # Even or Odd
 # Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 def even_or_odd(number):
@@ -6,11 +10,37 @@ def even_or_odd(number):
     else:
         return "Odd"
 
+
 # Example usage:
 result = even_or_odd(7)
 print(result)  # Output: Odd
 
 # ****************************************************************************************************
+print("****************************************************************************************************")
+
+
+# Multiply
+# This code does not execute properly. Try to figure out why.
+def multiply(a, b):
+    return a * b
+
+
+multiply_result = multiply(7, 3)
+print(multiply_result)
+
+# ****************************************************************************************************
+print("****************************************************************************************************")
+
+
+# Sum of positive
+# You get an array of numbers, return the sum of all of the positives ones.
+# Example [1,-4,7,12] => 1 + 7 + 12 = 20
+# Note: if there is nothing to sum, the sum is default to 0.
+
+def positive_sum(arr):
+    # Your code here
+    return 0
 
 
 # ****************************************************************************************************
+print("****************************************************************************************************")
